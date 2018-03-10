9b192c101933be4dda486de98c965d182c04920f
knod <knodding@gmail.com>
2018-03-10 10:02:30 -0500

    Basic carousel example

--- src/Containers/JourneyManager.js
+++ src/Containers/JourneyManager.js
@@ -1,51 +1,46 @@
-import React, { Component, Dimensions } from 'react';
-import {  Animated, WebView, View } from 'react-native';
+import React, { Component } from 'react';
+import {  Animated, WebView, View, Dimensions, Text } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
+
+  
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
 const styles = {
-  container:  { width: 200, height: 200, display: 'flex', flexDirection: 'row', overflow: 'scroll' },
-  vid:        { width: 200, height: 200, position: 'absolute' },
-  left:       { backgroundColor: 'green', top: 0, left: -200 },
-  center:     { backgroundColor: 'teal', top: 0, left: 0 },
-  right:      { backgroundColor: 'blue', top: 0, left: 200 }
+    container:  { width: 200, height: 200, display: 'flex', flexDirection: 'row', overflow: 'scroll' },
+    vid:        { width: 200, height: 200 },
+    left:       { backgroundColor: 'green', top: 0, left: -200 },
+    center:     { backgroundColor: 'teal', top: 0, left: 0 },
+    right:      { backgroundColor: 'blue', top: 0, left: 200 }
 };
 
 class JourneyManager extends Component {
-  state = {left: new  Animated.Value(-200)}
-
-  componentDidMount () {
-    Animated.spring(
-      this.state.left,
-      { toValue: 0 }
-    ).start();
-  }
-
-  render() {
-    var left = this.state.left;
-
-    var left = {...styles.vid, ...styles.left, left: left},
-        center = {...styles.vid, ...styles.center},
-        right = {...styles.vid, ...styles.right};
-
-    return (
-      <View style={styles.container}>
-        <View style={center} />
-        <Animated.View style={left} />
-        <View style={right} />
-      </View>
 
 
-    //   <WebView
-    //       ref={'webview'}
-    //       automaticallyAdjustContentInsets={false}
-    //       source={{uri: 'https://github.com/facebook/react-native'}}
-    //       javaScriptEnabled={true}
-    //       domStorageEnabled={true}
-    //       decelerationRate="normal"
-    //       startInLoadingState={true}
-    //       style={{ width: 200, height: 20 }}  />
-    );
-  }
+
+    state = { entries: [{color: 'blue'}, {color: 'green'}, {color: 'teal'}]}
+
+    _renderItem ({item, index}) {
+
+        var style = {...styles.vid, backgroundColor: item.color}
+
+        return (
+            <View style={style}>
+                <Text>{ 'Here' }</Text>
+            </View>
+        );
+    }
+
+    render () {
+        return (
+            <Carousel
+              ref={(c) => { this._carousel = c; }}
+              data={this.state.entries}
+              renderItem={this._renderItem}
+              sliderWidth={200}
+              itemWidth={190} />
+        );
+    }
 }
 
 export {
